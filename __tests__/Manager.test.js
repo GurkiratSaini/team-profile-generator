d4c9df4706f485642ff2046e1206257e8b375c35
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-04-20 19:02:20 -0400

    added tests for Engineer and Intern classes, created Engineer and Intern classes

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -14,3 +14,27 @@ test('gets manager role', () => {
 
     expect(manager.getRole()).toBe('Manager');
 })
+
+test('gets manager name', () => {
+    const manager = new Manager('Dave', 5, 'dave@gmail.com', 2);
+
+    expect(manager.getName()).toBe('Dave');
+})
+
+test('gets manager ID', () => {
+    const manager = new Manager('Dave', 5, 'dave@gmail.com', 2);
+
+    expect(manager.getId()).toBe(5);
+})
+
+test('gets manager email', () => {
+    const manager = new Manager('Dave', 5, 'dave@gmail.com', 2);
+
+    expect(manager.getEmail()).toBe('dave@gmail.com');
+})
+
+test('gets manager role', () => {
+    const manager = new Manager('Dave', 5, 'dave@gmail.com', 2);
+
+    expect(manager.getRole()).toBe('Manager');
+})
